949f7dc01a98d9326dddb5f496e8c54701d6b389
j-hai <115841204+j-hai@users.noreply.github.com>
2025-01-11 18:48:27 +0000

    Deploying to gh-pages from @ j-hai/j-hai.github.io@3d9b92121d288c7d4c7330e2a0e937384559ea2e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "Members of the Research Group and Alumni",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "dropdown-publications",
               title: "publications",
               description: "",
